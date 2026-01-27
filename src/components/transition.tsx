68f615c004f2c9e8d014d6bcec7a99aca3cbb195
佐々木 飛羽 <s2421183@s.do-johodai.ac.jp>
2026-01-27 10:33:04 +0900

    ロードアニメーション再修正

--- src/components/transition.tsx
+++ src/components/transition.tsx
@@ -1,36 +1,30 @@
-import { useEffect, useState } from "react";
 import { useLocation, Routes } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
 import { Loading } from "./loading";
 
 export const Transition = ({ children }: { children: React.ReactNode }) => {
   const location = useLocation();
-
-  const [displayLocation, setDisplayLocation] = useState(location);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (location.key !== displayLocation.key) {
-      setLoading(true);
+    setLoading(true);
 
-      const timer = setTimeout(() => {
-        setDisplayLocation(location);
-        setLoading(false);
-      }, 800);
-      return () => clearTimeout(timer);
-    }
-  }, [location, displayLocation]);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 800);
+
+    return () => clearTimeout(timer);
+  }, [location.key]);
 
   return (
     <>
-      {/* 転移アニメーション */}
       <AnimatePresence>
         {loading && <Loading />}
       </AnimatePresence>
 
-      {/* ページ本体 */}
       <AnimatePresence mode="wait">
-        <Routes location={displayLocation}>
+        <Routes location={location}>
           {children}
         </Routes>
       </AnimatePresence>
