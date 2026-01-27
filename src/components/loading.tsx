666ccbc5a7c0b29db69d682fdfd1bed9aec72fb0
佐々木 飛羽 <s2421183@s.do-johodai.ac.jp>
2026-01-27 11:34:26 +0900

    ロードアニメーション再調整

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -5,7 +5,7 @@ export const Loading = () => {
   return (
     <motion.div
       className="fixed inset-0 z-50 flex items-center justify-center bg-white text-white"
-      initial={{ opacity: 0 }}
+      initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
